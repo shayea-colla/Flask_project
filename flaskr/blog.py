e24b3799379c6d13f7cf11d04684e4f73d65a925
samy <samysam204o@gmail.com>
2023-01-23 09:37:14 +0300

    finishing the project backend and front end 'primarly'

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -1,56 +1,58 @@
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for,
-    )
+    Blueprint,
+    flash,
+    g,
+    redirect,
+    render_template,
+    request,
+    url_for,
+)
 from .db import get_db
 from .auth import login_required
 
 from werkzeug.exceptions import abort
 
 
-bp = Blueprint('blog', __name__)
-
-
-
-
+bp = Blueprint("blog", __name__)
 
 
 @bp.route("/")
 def index():
     db = get_db()
     posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' ORDER BY created DESC'
-        ).fetchall()
+        "SELECT p.id, title, body, created, author_id, username"
+        " FROM post p JOIN user u ON p.author_id = u.id"
+        " ORDER BY created DESC"
+    ).fetchall()
     return render_template("blog/index.html", posts=posts)
 
-@bp.route('/create', methods=("GET", "POST"))
+
+@bp.route("/create", methods=("GET", "POST"))
 @login_required
 def create():
-    
+
     if request.method == "POST":
         title = request.form["title"]
-        body  = request.form["body"]
-        
+        body = request.form["body"]
+
         db = get_db()
         error = None
-        
+
         if not title:
             error = "title is required"
         if not body:
             error = "body is required"
-        
-        if error == None:
+
+        if error is not None:
+            flash(error)
+        else:
             db.execute(
-                'INSERT INTO post (title, body, author_id) VALUES (?,?,?)',
-                (title, body, g.user['id'])
+                "INSERT INTO post (title, body, author_id) VALUES (?,?,?)",
+                (title, body, g.user["id"]),
             )
             db.commit()
-            return redirect(url_for('index'))
-        
-        flash(error)
+            return redirect(url_for("index"))
 
-    
     return render_template("blog/create.html")
 
 
@@ -58,47 +60,57 @@ def create():
 @login_required
 def update(id):
     post = get_post(id)
-    
+
     if request.method == "POST":
         title = request.form["title"]
-        body  = request.form["body"]
+        body = request.form["body"]
         db = get_db()
         error = None
-        
+
         if not title:
             error = "title is required"
-        if not body: 
+        if not body:
             error = "body is required"
-        
-        if error == None:
+
+        if error != None:
+            flash(error)
+        else:
             db.execute(
-                "UPDATE post SET title = ?, body = ? "
-                "WHERE id = ?", 
-                (title, body , id)
+                "UPDATE post SET title = ?, body = ? " "WHERE id = ?", (title, body, id)
             )
             db.commit()
-    else:
-        return render_template('blog/update.html', post=post)
+            return redirect(url_for("blog.index"))
+
+    return render_template("blog/update.html", post=post)
 
 
+@bp.route("/delete/<int:id>", methods=("POST",))
+@login_required
+def delete(id):
 
-
+    get_post(id)
+    db = get_db()
+    db.execute("DELETE FROM post WHERE id = ?", (id,))
+    db.commit()
+    return redirect(url_for("blog.index"))
 
 
 def get_post(id, check_author=True):
-    post = get_db().execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' WHERE p.id = ?',
-        (id,)
-    ).fetchone()
+    post = (
+        get_db()
+        .execute(
+            "SELECT p.id, title, body, created, author_id, username"
+            " FROM post p JOIN user u ON p.author_id = u.id"
+            " WHERE p.id = ?",
+            (id,),
+        )
+        .fetchone()
+    )
 
     if post == None:
         abort(404, f"post id {id} doesn't exist")
-    
-    if check_author and post['author_id'] != g.user['id']:
+
+    if check_author and post["author_id"] != g.user["id"]:
         abort(403)
-    
+
     return post
-
-
